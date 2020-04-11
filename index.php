e0d5cc9b5f2cbdda9138636de20ba0e8706ba848
kildarykjc <55935221+kildarykjc@users.noreply.github.com>
2020-04-10 21:38:48 -0300

    Torre de Hanói criação

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+
+<?php 
+// Função que recebe o numero de discos que deve ser acima de zero.
+ function hanoi($n){
+	$minJogadas;
+	if($n > 2){
+		 $minJogadas = (2*$n)+1;
+	}else if ($n==1){
+		 $minJogadas = 1;
+	}else if ($n==2){
+		 $minJogadas = 3;
+	 }else {
+		 $minJogadas = 'Valor de disco incorreto';
+	 }
+	 return $minJogadas;
+ }
+  
+ 
+?>
+
+
+<head>
+	
+	<meta charset="UTF-8">
+	<title>Torre de Hanói</title>
+	
+</head>
+
+<body >
+	<div>
+		<form action="?a=ok" method="POST">
+			<input type="text" name="texto" placeholder='Número de discos'/>
+			<input type="submit" value="Verificar" />
+		</form>
+	</div>
+	<?php
+		// Verifica se existe a ação e verifica se a ação foi igual a 'ok' e se o texto é diferente de vazio.
+		if ( isset( $_GET['a'] ) && $_GET['a'] == 'ok' && $_POST['texto'] != '' ) {
+			$discos = $_POST['texto'] ;
+			echo "Número mínimo de jogadas para $discos discos: ".hanoi($discos);
+		}
+	?>
+	
+</body>
+</html>
